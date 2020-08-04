52188231ee68157a35c81b906af128bfef26f3d4
Michael Romanoff <2michael.romanoff@gmail.com>
2020-08-04 00:35:14 -0700

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+// DO NOT REMOVE: this is used by webstorm for autocompletion when importing with '~' prefix
+
+module.exports = {
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+};
